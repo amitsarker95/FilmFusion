fa44f9db6cae7639d7ca669d0566dc5fc21f346a
amit <amitsarker95@gmail.com>
2025-01-26 12:33:35 +0600

    movie-details view tested

--- watchlist/views.py
+++ watchlist/views.py
@@ -5,8 +5,20 @@ from django.http import JsonResponse
 
 def movie_list(request):
     movies = Movies.objects.all()
-    data = {'movies' : list(movies.values())}
+    data = {
+        'movies' : list(movies.values())
+        }
 
     return JsonResponse(data)
 
+
+def movie_details(request, pk):
+    movie = Movies.objects.get(pk=pk)
+    data = {
+        'title' : movie.title,
+        'description' : movie.description,
+        'active' : movie.active
+    }
+
+    return JsonResponse(data)
 
