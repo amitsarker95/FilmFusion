dd92c243305c4aaf8e2ad26f0c2fd7802203df23
amit <amitsarker95@gmail.com>
2025-02-02 11:27:23 +0600

    mute a module

--- watchlist/api/views.py
+++ watchlist/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import mixins, generics
+from rest_framework import generics
 from rest_framework.views import APIView
 
 from watchlist.models import WatchList, StreamPlatform, Review
