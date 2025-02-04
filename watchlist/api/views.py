d7b372480003f25eb012fd022c702558396c3b79
amit <amitsarker95@gmail.com>
2025-02-04 13:45:36 +0600

    fixing some errors and check data using postman

--- watchlist/api/views.py
+++ watchlist/api/views.py
@@ -97,46 +97,6 @@ class StreamPlatformViewSet(ViewSet):
         return Response({'delete': "Object has been deleted"}, status=status.HTTP_204_NO_CONTENT)
 
 
-# class StreamPlatformApiView(APIView):
-#     def get(self, request):
-#         platform = StreamPlatform.objects.all()
-#         serializer = StreamPlatformSerializer(platform, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-    
-#     def post(self, request):
-#         serializer = StreamPlatformSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-
-# class StreamPlatformDetailApiView(APIView):
-    
-#     def get(self, request, pk):
-#         try:
-#             platform = StreamPlatform.objects.get(pk=pk)
-#         except StreamPlatform.DoesNotExist:
-#             return Response({'Error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
-        
-#         serializer = StreamPlatformSerializer(platform)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-    
-#     def put(self, request, pk):
-#         try:
-#             platform = StreamPlatform.objects.get(pk=pk)
-#         except StreamPlatform.DoesNotExist:
-#             return Response({'Error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
-#         serializer = StreamPlatformSerializer(platform, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-#     def delete(self, request, pk):
-#         platform = StreamPlatform.objects.get(pk=pk)
-#         platform.delete()
-#         return Response({'delete': "Object has been deleted"}, status=status.HTTP_204_NO_CONTENT)
 
 
 class ReviewCreateView(generics.CreateAPIView):
