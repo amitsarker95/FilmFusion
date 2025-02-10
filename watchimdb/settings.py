ed3775a8ada3ea2fe02526d846b7b61fc6b6fbd3
amit <amitsarker95@gmail.com>
2025-02-10 23:28:13 +0600

    make some change

--- watchimdb/settings.py
+++ watchimdb/settings.py
@@ -137,6 +137,6 @@ DJOSER = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=2),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
