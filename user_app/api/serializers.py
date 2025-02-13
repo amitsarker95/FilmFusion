615b9d946c4832a3e5678d662ba98bcc550976f4
amit <amitsarker95@gmail.com>
2025-02-14 00:15:24 +0600

    Override save method in registration serializer

--- user_app/api/serializers.py
+++ user_app/api/serializers.py
@@ -12,3 +12,22 @@ class RegistrationSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password' : {'write_only': True}
         }
+
+    def save(self, *args, **kwargs):
+        
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        user = User.objects.filter(email= self.validated_data['email'])
+
+        if password != password2:
+            raise serializers.ValidationError({'error' : 'Password and confirm password are not the same'})
+        if user.exists():
+            raise serializers.ValidationError({'error' : 'This email already exists'})
+        
+        account = User.objects.create(email=self.validated_data['email'], username=self.validated_data['username'])
+        account.set_password(password)
+        account.save()
+
+        return account
+        
