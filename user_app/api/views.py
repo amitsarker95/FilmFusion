0e968e3a01743b58878ae2e896071179f70df7aa
amit <amitsarker95@gmail.com>
2025-02-14 23:20:47 +0600

    COMMENT

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+#MODELS & SERIALIZERS
 from .serializers import RegistrationSerializer
 
 
