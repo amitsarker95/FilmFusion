df86aabd1fb958335c6399a93a2a5db772a07cd1
amit <amitsarker95@gmail.com>
2025-02-20 14:10:24 +0600

    added LogOutView in User_app

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 #MODELS & SERIALIZERS
 from .serializers import RegistrationSerializer
 
@@ -16,3 +18,18 @@ class RegestrationView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class LogOutView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        try:
+            refresh_token = request.data.get('refresh')
+
+            if not refresh_token:
+                return Response({"error": "Refresh token is required"}, status=status.HTTP_400_BAD_REQUEST)
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"success": "You have successfully logout from your account"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e) }, status=status.HTTP_400_BAD_REQUEST)
