55d0cbe286bd2891374077b15f559210c02b3580
amit <amitsarker95@gmail.com>
2025-02-20 14:14:18 +0600

    added logout url

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegestrationView
+from .views import RegestrationView, LogOutView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -10,5 +10,6 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('registration/', RegestrationView.as_view(), name='registration')
+    path('registration/', RegestrationView.as_view(), name='registration'),
+    path('logout/', LogOutView.as_view(), name='logout'),
 ]
