a541d6dec78d00b545abc28de29cfc726ce5e009
amit <amitsarker95@gmail.com>
2025-02-20 12:10:08 +0600

    change a url name

--- watchimdb/settings.py
+++ watchimdb/settings.py
@@ -139,6 +139,6 @@ DJOSER = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=2),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
 }
--- watchimdb/urls.py
+++ watchimdb/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('watchlist/', include('watchlist.api.urls')),
+    path('watch/', include('watchlist.api.urls')),
     path('account/', include('user_app.api.urls')),
     
 ]
